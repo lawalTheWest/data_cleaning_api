6d29a47ebb8615bff72df23a75fc40397c7f86ad
Tajudeen <lwltjdn@gmail.com>
2024-08-03 16:17:28 +0100

    a module to find a string in a file

---
+++ utils/_find.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' File I/O for word search '''
+
+import argparse
+
+def find_me(self, word):
+    '''
+        finds a word in a text and
+        counts occurences
+    '''
+    pass
