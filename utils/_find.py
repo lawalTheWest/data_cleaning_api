b68b245e9e98833c9ce79b5b0190f53c1ab60278
Tajudeen <lwltjdn@gmail.com>
2024-08-03 16:38:06 +0100

    update

--- utils/_find.py
+++ utils/_find.py
@@ -12,4 +12,24 @@ def find_me(self, word):
         finds a word in a text and
         counts occurences
     '''
-    pass
+    word = word.lower()
+    occurences = 0
+    lines_found = []
+
+    with open(self, 'r') as f:
+        '''
+            iterate through the lines to:
+                find and
+                count occurences
+        '''
+        for line_number, line in enumerate(f, start=1):
+            '''
+                Convert lines to lower case
+            '''
+            line_lower = line.lower()
+            if word in line_lower:
+                # count occurences
+                count_in_line = line_lower.count(word)
+                occurences += count_in_line
+                # record line number and counts
+                lines_found.apped((line_number, count_in_line))
